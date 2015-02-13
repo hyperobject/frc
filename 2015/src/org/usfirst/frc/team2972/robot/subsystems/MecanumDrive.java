8b825ac85be849c46f724468751a25052cb4f559
Connor <technoboy10@fake.email>
2015-02-12 17:16:15 -0700

    Method stub for drive

--- 2015/src/org/usfirst/frc/team2972/robot/subsystems/MecanumDrive.java
+++ 2015/src/org/usfirst/frc/team2972/robot/subsystems/MecanumDrive.java
@@ -9,6 +9,12 @@ public class MecanumDrive extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+	public void drive(double angle){
+		/*
+		 * TODO
+		 * drive at an angle 0-360
+		 */
+	}
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
