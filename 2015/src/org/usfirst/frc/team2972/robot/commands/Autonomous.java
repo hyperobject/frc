340b8ab107ac72c224778cb1487c9ac770bfeeed
Connor <technoboy10@fake.email>
2015-02-14 15:26:34 -0700

    DriveUntilTote and autonomous

--- 2015/src/org/usfirst/frc/team2972/robot/commands/Autonomous.java
+++ 2015/src/org/usfirst/frc/team2972/robot/commands/Autonomous.java
@@ -24,5 +24,6 @@ public class Autonomous extends CommandGroup {
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
+    	addSequential
     }
 }
---
+++ 2015/src/org/usfirst/frc/team2972/robot/commands/DriveUntilTote.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team2972.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveUntilTote extends CommandBase {
+    private double speed = 0.3;
+    public DriveUntilTote() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	super("DriveUntilTote");
+    	requires(mecanumDrive);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	mecanumDrive.drive(speed, 0, 0);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return (ultrasonicDepth < 10);
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
