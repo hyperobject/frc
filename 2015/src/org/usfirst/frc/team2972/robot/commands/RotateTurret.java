a01e92d91ae50004597d197498c1f2424cbf7f55
Connor <technoboy10@fake.email>
2015-02-14 16:12:58 -0700

    Finished StoreTote stubs

---
+++ 2015/src/org/usfirst/frc/team2972/robot/commands/RotateTurret.java
@@ -0,0 +1,49 @@
+package org.usfirst.frc.team2972.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class RotateTurret extends CommandBase {
+    private int direction;
+    public RotateTurret(int dir) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	super("RotateTurret");
+    	requires(turret);
+    	direction = dir;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	if (direction == -1) {
+    		turret.turnRight();
+    	} else if (direction == 1) {
+    		turret.turnLeft();
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        if (direction == 1){
+        	//if totally forward again
+        } else if (direction == -1) {
+        	// if totally back
+        }
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
