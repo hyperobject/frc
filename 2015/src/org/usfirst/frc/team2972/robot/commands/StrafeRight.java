20b600fe3e56c67e76a9a9a558cd3ffd4a083379
ConnerJLewis <crlewis@dawsonstudents.org>
2015-02-14 16:06:03 -0700

    Added the movement command group and strafe right command

---
+++ 2015/src/org/usfirst/frc/team2972/robot/commands/StrafeRight.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team2972.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ *
+ */
+public class StrafeRight extends CommandBase {
+	private double speed = 0.3;
+    public StrafeRight() {
+    	super("StrafeRight");
+    	requires(mecanumDrive);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	mecanumDrive.drive( 0, 0, speed);
+    	Timer.delay(5);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	mecanumDrive.drive( 0, 0, 0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
