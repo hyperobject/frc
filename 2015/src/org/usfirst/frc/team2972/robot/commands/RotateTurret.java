3d05085a1a0fece39287ba90be1ef9bfb1811f2b
Connor <technoboy10@fake.email>
2015-02-16 12:01:24 -0700

    asdfghjkjhgfdssdfhjkjgfdsdfhjkjgfdsdfghjydsdfghjhgfdxcghjugfdfghjhgcx bnjhgfc mjygfvc nmkjuytfv nkuytfvbnmkuytgfvbnmkiuytgbnkiuytgbnjuytgfv nmjuygbnmkiuytfvb nmkuygv mkuygfv mkjuygfv mjhb ,kuytredftyuytre4567890

--- 2015/src/org/usfirst/frc/team2972/robot/commands/RotateTurret.java
+++ 2015/src/org/usfirst/frc/team2972/robot/commands/RotateTurret.java
@@ -23,11 +23,7 @@ public class RotateTurret extends CommandBase {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	if (direction == -1) {
-    		turret.turnRight();
-    	} else if (direction == 1) {
-    		turret.turnLeft();
-    	}
+    	turret.turnLeft();
     }
 
     // Make this return true when this Command no longer needs to run execute()
