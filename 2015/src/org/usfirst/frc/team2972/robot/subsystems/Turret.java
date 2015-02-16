3689d24fd91962be775ffb224ccbd4a1b18f1947
Alex Pelletier <alexpell00@yahoo.com>
2015-02-16 10:53:25 -0700

    added potentiometer

--- 2015/src/org/usfirst/frc/team2972/robot/subsystems/Turret.java
+++ 2015/src/org/usfirst/frc/team2972/robot/subsystems/Turret.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team2972.robot.subsystems;
 import org.usfirst.frc.team2972.robot.RobotMap;
 import org.usfirst.frc.team2972.robot.commands.TurretCommand;
 
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -12,6 +13,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Turret extends Subsystem {
     
 	public static Talon turretMotor = new Talon(RobotMap.turretMotorPort);
+	public static AnalogInput potentiometer  = new AnalogInput(0);
 	
     double speed = 1;
     
@@ -35,6 +37,10 @@ public class Turret extends Subsystem {
     
     public void stop(){
     	turretMotor.set(0);
+    }
+    
+    public int getAnologInput(){
+    	return potentiometer.getValue();
     }
     
     public void turn(double dir){
