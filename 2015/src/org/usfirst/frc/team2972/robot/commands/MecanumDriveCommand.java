367440d9fc98748e262fc4b3249d4c83b99ee47b
Connor <technoboy10@fake.email>
2015-02-17 16:01:54 -0700

    aaaaaaargh

--- 2015/src/org/usfirst/frc/team2972/robot/commands/MecanumDriveCommand.java
+++ 2015/src/org/usfirst/frc/team2972/robot/commands/MecanumDriveCommand.java
@@ -20,6 +20,8 @@ public class MecanumDriveCommand extends CommandBase {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() { // 0 , 1 
+    	double[] axes = {OI.getStick(1).getRawAxis(0), OI.
+    	
     	mecanumDrive.drive(Math.pow(OI.getStick(1).getRawAxis(0),2), Math.pow(OI.getStick(1).getRawAxis(1),2), (OI.getStick(1).getRawAxis(3) - OI.getStick(1).getRawAxis(2)));
     }
 
